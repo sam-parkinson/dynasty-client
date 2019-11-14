4b89d01b42b5cd192626e2ad4235511d6da5d042
Sam Parkinson <parkinsonsp42@gmail.com>
2019-11-14 15:00:28 -0500

    Add base header, footer components

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Content to be added in future updates</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>The League</h1>
+        <nav>
+          This is where the navbar will go
+        </nav>
+      </header>
+    )
+  }
+}
