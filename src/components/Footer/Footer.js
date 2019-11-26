c73fa189aa9128e66306960236b4136aa305381a
Sam Parkinson <parkinsonsp42@gmail.com>
2019-11-26 12:11:07 -0500

    Update footer to remain at bottom of page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
+function FooterLink(props) {
+  return (
+    <li><a href={props.link} target="_blank">{props.text}</a></li>
+  )
+}
+
 export default class Footer extends Component {
   render() {
     return (
       <footer>
-        Footer text goes here
+        <ul className="footerInfo">
+          <FooterLink text="Repo" link="https://github.com/sam-parkinson/dynasty-client" />
+          <FooterLink text="My Portfolio" link="https://sam-parkinson.github.io/portfolio/" />
+        </ul>
       </footer>
     )
   }
